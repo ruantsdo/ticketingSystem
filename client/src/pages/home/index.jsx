0ff583d9daf6fedcdda630a3fa22b33010d77686
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2023-10-27 13:49:59 -0300

    cleaning
    cleaning unnecessary files

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,9 +1,3 @@
-//React
-import { useState } from "react";
-
-//Socket
-import * as socketIOClient from "socket.io-client";
-
 //Components
 import Container from "../../components/container";
 import NavBar from "../../components/navbar";
@@ -11,32 +5,11 @@ import NavBar from "../../components/navbar";
 //NextUI
 import {} from "@nextui-org/react";
 
-//Configs
-import { myIp, port } from "../../services/api";
-
 function Home() {
-  const [queueItems, setQueueItems] = useState([]);
-
-  const onNewQueueItem = (newQueueItem) => {
-    setQueueItems([...queueItems, newQueueItem]);
-  };
-
-  //const socket = socketIOClient.io(`http://${myIp}:${port}`);
-
-  //socket.connect();
-
-  //socket.on("newQueueItem", onNewQueueItem);
-
   return (
     <>
       <NavBar />
-      <Container>
-        {queueItems.map((queueItem) => (
-          <div key={queueItem.id}>
-            {queueItem.id} - {queueItem.name}
-          </div>
-        ))}
-      </Container>
+      <Container>Home Page</Container>
     </>
   );
 }
