40b19e7b0e0d2ad6f099badfbf8cdfb4ffe90dc7
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-04-24 14:54:10 -0300

    :sparkles: create store index

---
+++ client/src/stores/index.jsx
@@ -0,0 +1,17 @@
+/* Stores */
+import useUsersStore from "./usersStore/store";
+import useServicesStore from "./servicesStore/store";
+import useLocationsStore from "./locationsStore/store";
+/* Utils */
+import useUsersUtils from "./usersStore/utils";
+import useServicesUtils from "./servicesStore/utils";
+import useLocationsUtils from "./locationsStore/utils";
+/* Exports */
+export {
+  useUsersStore,
+  useUsersUtils,
+  useServicesStore,
+  useServicesUtils,
+  useLocationsStore,
+  useLocationsUtils,
+};
