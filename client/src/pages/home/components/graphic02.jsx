b01d495d337560c03bd2406789c05cddbdaa503c
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-24 14:33:13 -0300

    home page improved
    more detailed data is now shown to user

---
+++ client/src/pages/home/components/graphic02.jsx
@@ -0,0 +1,60 @@
+import { Cell, Legend, Pie, PieChart, ResponsiveContainer } from "recharts";
+
+const COLORS = ["#F5A524", "#2C931F"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
+
+const Graph02 = ({ graphData }) => (
+  <ResponsiveContainer width="100%" minHeight={300} height="100%">
+    <PieChart width={400} height={400}>
+      <Pie
+        data={graphData}
+        cx="50%"
+        cy="50%"
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="Quantidade"
+      >
+        {graphData.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={COLORS[index]} />
+        ))}
+      </Pie>
+      <Legend
+        payload={graphData.map((entry, index) => ({
+          id: entry.Nome,
+          type: "square",
+          value: entry.Nome,
+          color: COLORS[index],
+        }))}
+      />
+    </PieChart>
+  </ResponsiveContainer>
+);
+
+export default Graph02;
